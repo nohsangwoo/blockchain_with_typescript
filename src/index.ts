39c9e1a950963a9b76c855b07a8008caff2292f1
nohsangwoo <nsgr12@naver.com>
2020-10-13 22:09:46 +0900

    interface on typescript

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,22 @@
-const sayHi = (name: string, age: number, gender: string): string => {
+import { visitParameterList } from "typescript";
+
+interface Human {
+  name: string;
+  age: number;
+  gender?: string;
+}
+
+const person = {
+  name: "sangwoo",
+  age: 32,
+  gender: "male",
+};
+
+const sayHi = (person: Human): string => {
+  const { name, age, gender } = person;
   return `Hello ${name} , you are a ${age}, you are a ${gender}`;
 };
 
-console.log(sayHi("sangwoo", 32, "male"));
+console.log(sayHi(person));
 
 export {};
